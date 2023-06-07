a5be2d41d4e5b5a2a0c7c440821c40b1bb28e2f0
Vakkas Sahin <vakkassahin260@hotmail.com>
2023-06-07 23:28:30 +0300

    models file updated.

--- models/demands.go
+++ models/demands.go
@@ -6,9 +6,9 @@ import (
 
 // Demand struct represents a demand for a specific shift
 type Demand struct {
-	ID        uint       `gorm:"primaryKey" json:"id"`
-	UserID    uint       `gorm:"column:user_id" json:"userId"`
-	ShiftID   uint       `gorm:"column:shift_id" json:"shiftId"`
+	ID        int64      `gorm:"primaryKey" json:"id"`
+	UserID    int64      `gorm:"column:user_id" json:"userId"`
+	ShiftID   int64      `gorm:"column:shift_id" json:"shiftId"`
 	StartDate time.Time  `gorm:"column:start_date" json:"startDate"`
 	EndDate   time.Time  `gorm:"column:end_date" json:"endDate"`
 	CreatedAt time.Time  `gorm:"column:created_at" json:"createdAt"`
--- models/user_shift_period.go
+++ models/user_shift_period.go
@@ -6,11 +6,11 @@ import (
 
 // UserShiftPeriod struct represents the shift period of a user
 type UserShiftPeriod struct {
-	ID        uint       `gorm:"primaryKey" json:"id"`
-	UserID    uint       `gorm:"column:user_id" json:"userId"`
+	ID        int64      `gorm:"primaryKey" json:"id"`
+	UserID    int64      `gorm:"column:user_id" json:"userId"`
 	StartDate time.Time  `gorm:"column:start_date" json:"startDate"`
 	EndDate   time.Time  `gorm:"column:end_date" json:"endDate"`
-	ShiftID   uint       `gorm:"column:shift_id" json:"shiftId"`
+	ShiftID   int64      `gorm:"column:shift_id" json:"shiftId"`
 	CreatedAt time.Time  `gorm:"column:created_at" json:"createdAt"`
 	UpdatedAt time.Time  `gorm:"column:updated_at" json:"updatedAt"`
 	DeletedAt *time.Time `gorm:"column:deleted_at" json:"-"`
