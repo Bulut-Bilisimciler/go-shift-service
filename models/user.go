2904f6de589e1ba56a583a174016c7b28bc1c966
selcuksan <18360859049@ogrenci.btu.edu.tr>
2023-06-09 12:56:43 +0300

    user.go model updated

--- models/user.go
+++ models/user.go
@@ -5,11 +5,11 @@ import "time"
 type User struct {
 	ID         int64     `json:"id" gorm:"primary_key;not null"`
 	UserID     int64     `json:"user_id" gorm:"column:user_id"` // user id from your organization
-	Name       string    `json:"name" gorm:"column:name:"`
-	Surname    string    `json:"surname" gorm:"column:surname:"`
-	Email      string    `json:"email" gorm:"column:email:"`
-	Made_Field string    `json:"made_field"  gorm:"column:made_field:"`
-	Nickname   string    `json:"nickname"  gorm:"column:nickname:"`
+	Name       string    `json:"name" gorm:"column:name"`
+	Surname    string    `json:"surname" gorm:"column:surname"`
+	Email      string    `json:"email" gorm:"column:email"`
+	Made_Field string    `json:"made_field" gorm:"column:made_field"`
+	Nickname   string    `json:"nickname" gorm:"column:nickname"`
 	CreatedAt  time.Time `json:"created_at" gorm:"column:created_at"`
 	UpdatedAt  time.Time `json:"updated_at" gorm:"column:updated_at"`
 }
